f16da1011c969339c7e7799344da7d38a31260cb
سالم بن حيدر <salem22muk@gmail.com>
2025-10-27 18:46:28 +0000

    لم يستخدم الخط

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Cairo', 'sans-serif'],
-        headline: ['Cairo', 'serif'],
+        headline: ['Cairo', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
